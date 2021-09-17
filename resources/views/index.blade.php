e05c0435ae6d6d5c6daac3a41a08cceadcba18c6
nosfied <nosfied@gmail.com>
2021-09-17 15:07:58 -0300

    Inicio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.app')
+
+@section('body')
+
+<div class="jumbotron bg-light border border-secondary">
+    <div class="row">
+        <div class="card-deck">
+            <div class="card border border-primary">
+                <div class="card-body">
+                    <h5 class="card-title">Cadastro de Produtos</h5>
+                    <p class="card=text">
+                        Aqui você cadastra todos os seus produtos.
+                        Só não se esqueça de cadastrar previamente as categorias.
+                    </p>
+                    <a href="/produtos" class="btn btn-primary">Cadastre seus Produtos</a>
+                </div>
+            </div>
+            <div class="card border border-primary">
+                <div class="card-body">
+                    <h5 class="card-title">Cadastro de Produtos</h5>
+                    <p class="card=text">
+                        Aqui você cadastra todos os seus produtos.
+                        Só não se esqueça de cadastrar previamente as categorias.
+                    </p>
+                    <a href="/produtos" class="btn btn-primary">Cadastre suas Categorias</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
